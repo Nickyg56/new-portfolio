b902fbdfa6bc39d251dbedc681d4e925ccfa471a
Nicholas Gunter <ygnick@live.com>
2019-10-10 16:29:15 -0400

    Updates

---
+++ precache-manifest.a33c085b75eb88545383fe6871be1d9f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75eff84fc63e8c52951134f293f42292",
+    "url": "/new-portfolio/index.html"
+  },
+  {
+    "revision": "86950d939d89ccc857f0",
+    "url": "/new-portfolio/static/css/main.099fe081.chunk.css"
+  },
+  {
+    "revision": "a1a4fe0ea8e56d62f4f4",
+    "url": "/new-portfolio/static/js/2.feb605fb.chunk.js"
+  },
+  {
+    "revision": "86950d939d89ccc857f0",
+    "url": "/new-portfolio/static/js/main.02bd4946.chunk.js"
+  },
+  {
+    "revision": "b4b6e4b59efa1ffa2f16",
+    "url": "/new-portfolio/static/js/runtime-main.e15a1b8a.js"
+  },
+  {
+    "revision": "a274428d9f3082a0224a1b937d55e522",
+    "url": "/new-portfolio/static/media/BandBridge.a274428d.png"
+  },
+  {
+    "revision": "2f49c4be4a694a47f27761a9e719a5ad",
+    "url": "/new-portfolio/static/media/Nick.2f49c4be.jpg"
+  },
+  {
+    "revision": "d42a7916c411b114d340feddffdfce1e",
+    "url": "/new-portfolio/static/media/SpacedRep.d42a7916.png"
+  },
+  {
+    "revision": "8961eb232a52e2a33f15835016f0cb9d",
+    "url": "/new-portfolio/static/media/Sprout.8961eb23.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Nickyg56/new-portfolio/precache-manifest.d4fb3a1df6e8b65d39485b0e9e5a8b1c.js"
+  "/new-portfolio/precache-manifest.a33c085b75eb88545383fe6871be1d9f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Nickyg56/new-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/new-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
